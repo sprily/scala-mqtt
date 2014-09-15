852d1f662d386cf6bd708f4418738303d7a52a99
Ian Murray <ian.c.murray@gmail.com>
2014-09-15 17:45:05 +0100

    (core) Add logging to the paho connection

--- core/src/main/scala/internal/PahoMqttConnection.scala
+++ core/src/main/scala/internal/PahoMqttConnection.scala
@@ -138,22 +138,28 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
                 retained: Boolean) = ???
 
     def subscribe(to: Seq[(TopicPattern, QoS)]): Future[Unit] = {
+      logger.debug(s"Attempting to subscribe to: ${to}.  Awaiting lock first.")
       connLock.synchronized {
         if (!active) {
+          logger.warn("Unable to subscribe since connection is inactive.")
           Future.failed(new ActiveConnectionException())
         } else {
-          logger.debug(s"Subscribing to topics: ${to}")
+          logger.debug(s"Attempting to subscribe to topics: ${to}")
           try {
             val p = promise[Unit]
             val (topics, qoss) = to.unzip
             client.subscribe(topics.map(_.path),
                              qoss.map(_.value),
                              promiseAL(p))
+            logger.debug("Successfully sent subscription request")
             addInFlightSubscription(p)
             p.future andThen removeInFlightSubscription(p) andThen
                              addActiveSubscriptions(topics.zip(qoss))
           } catch {
-            case e: Exception => Future.failed(e)
+            case e: Exception => {
+              logger.warn(s"Error occurred attempting subscription: ${e}")
+              Future.failed(e)
+            }
           }
         }
       }
@@ -161,18 +167,24 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
 
     def unsubscribe(topics: Seq[TopicPattern]) = {
 
+      logger.debug(s"Attempting to unsubscribe from ${topics}.  Awaiting lock first.")
       connLock.synchronized {
         if (!active) {
+          logger.warn("Unable to unsubscribe since connection is inactive")
           Future.failed(new ActiveConnectionException())
         } else {
-          logger.debug(s"Unsubscribing from: ${topics}")
+          logger.debug(s"Attempting to unsubscribing from: ${topics}")
           try {
             val p = promise[Unit]
             client.unsubscribe(topics.map(_.path),
                                promiseAL(p))
+            logger.debug("Successfully sent un-subscription request")
             p.future andThen removeActiveSubscriptions(topics)
           } catch {
-            case e: Exception => Future.failed(e)
+            case e: Exception => {
+              logger.warn(s"Error occurred attempting un-subscription: ${e}")
+              Future.failed(e)
+            }
           }
         }
       }
@@ -188,8 +200,10 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
 
     /** Connect for the first time.  Fails early **/
     private[internal] def initialiseConnection(): Future[Unit] = {
+      logger.info("Attempting to intialise connection...")
       connLock.synchronized {
         if (active) {
+          logger.warn("Connection initialisation failed since connection is inactive")
           Future.failed(new ActiveConnectionException())
         } else {
           logger.debug("Activating MqttConnection")
@@ -209,17 +223,23 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
       * attempt itself fails.
       */
     private[this] def connect(): Future[Unit] = {
+      logger.debug("Attempting to connect to the broker.  Awaiting lock first")
       connLock.synchronized {
         if (!active) {
+          logger.warn("Unable to connect to broker since connection is invalid.")
           Future.failed(new InactiveConnectionException())
         } else {
           try {
             connectionState = Connecting
             val p = promise[Unit]
             client.connect(options, promiseAL(p))
+            logger.debug("Successfully initiated request to connect")
             p.future
           } catch {
-            case e: Exception => Future.failed(e)
+            case e: Exception => {
+              logger.warn(s"Attempt to connect failed: ${e}")
+              Future.failed(e)
+            }
           }
         }
       }
@@ -235,6 +255,7 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
       connLock.synchronized {
         logger.info("Connected to MQTT broker")
         connectionState = Connected
+        logger.debug("Notifying subscribers that we are connected")
         connectionStatusSubscriptions.notify(ConnectionStatus(true))
         resubscribeToActiveSubscriptions()
       }
@@ -244,13 +265,14 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
       * Called whenever the underlying connection is lost unexpectedly.
       */
     private[this] def handleUnexpectedDisconnect(t: Throwable) = {
+      logger.debug("Handling unexpected disconnection.  Awaiting lock first.")
       connLock.synchronized {
         logger.warn(s"Unexpected disconnection from MQTT broker: ${t}")
 
         if (connectionState != Connected) {
           logger.warn(s"Not attempting to re-connect, current state: ${connectionState}")
         } else {
-          logger.info(s"Handling unexpected disconnection from the broker.  Current state: ${connectionState}")
+          logger.info(s"Handling unexpected disconnection from the broker.")
           connectionState = Disconnected
           connectionStatusSubscriptions.notify(ConnectionStatus(false))
           failAllInFlightSubscriptions(t)
@@ -267,22 +289,23 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
       * it just plugging away at it.
       */
     private[this] def reconnect(): Future[Unit] = {
-        connect() recoverWith {
-          case e: paho.MqttException => {
-            logger.debug(s"Re-connection attempt failed: ${e}")
-            Thread.sleep(5.seconds.toMillis)
-            reconnect()
-          }
-          case e: InactiveConnectionException => {
-            logger.debug("Re-connection aborted since client has been de-activated")
-            Future.failed(e)
-          }
-          case e: Exception => {
-            logger.error(s"Caught unexpected error re-connecting to broker: ${e}")
-            Thread.sleep(5.seconds.toMillis)
-            reconnect()
-          }
+      logger.debug("Attempting to reconnect to broker")
+      connect() recoverWith {
+        case e: paho.MqttException => {
+          logger.debug(s"Re-connection attempt failed: ${e}")
+          Thread.sleep(5.seconds.toMillis)    // TODO: non-blocking
+          reconnect()
         }
+        case e: InactiveConnectionException => {
+          logger.debug("Re-connection aborted since client has been de-activated")
+          Future.failed(e)
+        }
+        case e: Exception => {
+          logger.error(s"Caught unexpected error re-connecting to broker: ${e}")
+          Thread.sleep(5.seconds.toMillis)    // TODO: non-blocking
+          reconnect()
+        }
+      }
     }
 
     /** Returns true if `e` indicates the connect was already broken **/
@@ -291,33 +314,44 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
     }
 
     private[this] def addInFlightSubscription(p: Promise[Unit]) = {
+      logger.debug("Adding in-flight subscription")
       inFlightSubscriptions.update (p :: _)
     }
 
     private[this] def failAllInFlightSubscriptions(t: Throwable) = {
       val (ps, _) = inFlightSubscriptions.update { _ => Nil }
 
+      logger.debug(s"Failing ${ps.length} in-flight subscriptions")
       // We don't really mind if the Promise has already been completed
       ps.foreach { p => p.tryFailure(new UnexpectedDisconnectionException(t)) }
     }
 
     private[this] def removeInFlightSubscription(p: Promise[Unit]): PartialFunction[Try[Unit], Unit] = {
-      case _ => inFlightSubscriptions.update(_.filter(_ != p))
+      case _ => {
+        logger.debug("Removing in-flight subscription")
+        inFlightSubscriptions.update(_.filter(_ != p))
+      }
     }
 
     private[this] def addActiveSubscriptions(ts: Seq[(TopicPattern, QoS)]): PartialFunction[Try[Unit], Unit] = {
       case Success(_) => {
+        logger.debug(s"Adding active subscriptions: ${ts}")
         activeSubscriptions.update { topics => topics |+| ts.toMap }
+        logger.debug(s"Active subscriptions after addition: ${activeSubscriptions.get}")
       }
     }
 
     private[this] def removeActiveSubscriptions(ts: Seq[TopicPattern]): PartialFunction[Try[Unit], Unit] = {
       case Success(_) => {
+        logger.debug(s"Removing active subscriptions: ${ts}")
         activeSubscriptions.update { _.filterKeys { t => ts contains t } }
+        logger.debug(s"Active subcriptions after removal: ${activeSubscriptions.get}")
       }
     }
 
     private[this] def resubscribeToActiveSubscriptions() = {
+
+      logger.debug("Re-subscribing to active subscriptions if necessary")
 
       def resubscribe(): Future[Unit] = {
         val subs = activeSubscriptions.get.toList
@@ -342,6 +376,7 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
             }
           }
         } else {
+          logger.debug(s"No active subscriptions to re-subscribe to: ${subs}")
           Future.successful(())
         }
       }
@@ -351,7 +386,7 @@ protected[mqtt] trait PahoMqttConnectionModule extends MqttConnectionModule[Futu
 
     /************ paho.MqttCallback implementation ************/
     def connectionLost(t: Throwable) = handleUnexpectedDisconnect(t)
-    def deliveryComplete(token: paho.IMqttDeliveryToken): Unit = ???
+    def deliveryComplete(token: paho.IMqttDeliveryToken): Unit = { }
 
     def messageArrived(topic: String, pMsg: paho.MqttMessage): Unit = {
       logger.debug(s"Message arrived at: ${topic}")
