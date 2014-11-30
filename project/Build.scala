311081939bdd783129f8c8e7e6dd08256200ae0f
Ian Murray <ian.c.murray@gmail.com>
2014-11-30 10:37:53 +0000

    (core) Switch to officially released paho client 1.0.1
    The Sprily patched release is redundant now that 1.0.1 is officially
    released.

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object ScalaMqtt extends Build {
     resolvers ++= Seq(
       "TypeSafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       "spring"            at "http://repo.springsource.org/plugins-release",
-      "sprily"            at "http://repo.sprily.co.uk/nexus/content/repositories/releases/"
+      "Paho Releases"     at "https://repo.eclipse.org/content/repositories/paho-releases"
     ),
 
     // Standardise some common dependencies.
@@ -21,7 +21,7 @@ object ScalaMqtt extends Build {
       "ch.qos.logback"              % "logback-classic"             % "1.1.2",
       "org.scalaz"                 %% "scalaz-core"                 % "7.0.6",
       "org.typelevel"              %% "scalaz-contrib-210"          % "0.1.5",
-      "uk.co.sprily"                % "org.eclipse.paho.client.mqttv3" % "1.0.0",
+      "org.eclipse.paho"            % "org.eclipse.paho.client.mqttv3" % "1.0.1",
       "org.scalacheck"             %% "scalacheck"                  % "1.11.1"           % "test",
       "org.scalatest"               % "scalatest_2.10"              % "2.2.1"            % "test"
     ),
